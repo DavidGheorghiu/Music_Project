b3ce3a72ef1ca612d581ff3ead272d131343073a
basil roy <basil.k.roy@hotmail.com>
2019-11-04 18:54:35 +0000

    hello

--- music_js/scripts.js
+++ music_js/scripts.js
@@ -1,21 +1,21 @@
 var cMajor = [
-    261.6,  // C
-    293.7,  // D
-    329.7,  // E
-    349.2,  // F
-    392.0,  // G
-    440.0,  // A
-    493.0,  // B
-    523.3   // C'
+  261.6, // C
+  293.7, // D
+  329.7, // E
+  349.2, // F
+  392.0, // G
+  440.0, // A
+  493.0, // B
+  523.3 // C'
 ];
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
 var osc = audioContext.createOscillator();
-osc.type = 'sine';
+osc.type = "triangle";
 osc.connect(audioContext.destination);
 
 function playSound(freq) {
-    osc.frequency.value = freq;
-    osc.start(0);
+  osc.frequency.value = freq;
+  osc.start(0);
 }
