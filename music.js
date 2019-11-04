c1683fa3510b2865b661978823e27e80784e0aa7
basil roy <basil.k.roy@hotmail.com>
2019-11-04 14:19:09 +0000

    david is geniuos

--- music.js
+++ music.js
@@ -1,3 +1,119 @@
+/*var context = new AudioContext();
 playSound = () => {
-  alert("Clicked");
+  var source = context.createBufferSource();
+  source.buffer = dogBarkingBuffer;
+  source.connect(context.destination);
+  source.start(0);
 };
+*/
+
+var context;
+window.addEventListener("load", init, false);
+function init() {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    context = new AudioContext();
+  } catch (e) {
+    alert("Web Audio API is not supported in this browser");
+  }
+}
+
+var dogBarkingBuffer = null;
+// Fix up prefixing
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
+function loadDogSound(url) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  // Decode asynchronously
+  request.onload = function() {
+    context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        dogBarkingBuffer = buffer;
+      },
+      onError
+    );
+  };
+  request.send();
+}
+
+// Fix up prefixing
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
+function playSound(buffer) {
+  var source = context.createBufferSource(); // creates a sound source
+  source.buffer = buffer; // tell the source which sound to play
+  source.connect(context.destination); // connect the source to the context's destination (the speakers)
+  source.start(0); // play the source now
+  // note: on older systems, may have to use deprecated noteOn(time);
+}
+
+window.onload = init;
+var context;
+var bufferLoader;
+
+function init() {
+  // Fix up prefixing
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  context = new AudioContext();
+
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      "../sounds/hyper-reality/br-jam-loop.wav",
+      "../sounds/hyper-reality/laughter.wav"
+    ],
+    finishedLoading
+  );
+
+  bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+  // Create two sources and play them both together.
+  var source1 = context.createBufferSource();
+  var source2 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
+
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source1.start(0);
+  source2.start(0);
+}
+
+for (var bar = 0; bar < 2; bar++) {
+  var time = startTime + bar * 8 * eighthNoteTime;
+  // Play the bass (kick) drum on beats 1, 5
+  playSound(kick, time);
+  playSound(kick, time + 4 * eighthNoteTime);
+
+  // Play the snare drum on beats 3, 7
+  playSound(snare, time + 2 * eighthNoteTime);
+  playSound(snare, time + 6 * eighthNoteTime);
+
+  // Play the hi-hat every eighth note.
+  for (var i = 0; i < 8; ++i) {
+    playSound(hihat, time + i * eighthNoteTime);
+  }
+}
+function playSound(buffer, time) {
+  var source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(context.destination);
+  source.start(time);
+}
+
+var gainNode = context.createGain();
+// Connect the source to the gain node.
+source.connect(gainNode);
+// Connect the gain node to the destination.
+gainNode.connect(context.destination);
+
+gainNode.gain.value = 0.5;
