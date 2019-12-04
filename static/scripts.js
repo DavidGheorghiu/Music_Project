855af6674e08dc73373788b769d4d878d25ad4c7
DavidGheorghiu <Pvt.david@yahoo.com>
2019-12-04 22:44:01 +0000

    added sequencer and the ability to loop through it

--- static/scripts.js
+++ static/scripts.js
@@ -16,3 +16,29 @@ function loop() {
 function stop() {
     Tone.Transport.stop();
 }
+
+var sequencer = document.querySelector('.sequencer');
+sequencer.addEventListener('click', function(event) {
+    eventTarget = event.target;
+    if(eventTarget.className.includes('cell')) {
+        classlist = eventTarget.classList;
+        if(!classlist.contains('selected')) {
+            classlist.add('selected');
+        }
+        else {
+            classlist.remove('selected');
+        }
+    }
+});
+
+function play() {
+    var columns = document.querySelectorAll('.column');
+    for(let column of columns) {
+        var cells = Array.prototype.slice.call(column.children);
+        for(let cell of cells) {
+            if(cell.classList.contains('selected')) {
+                
+            }
+        }
+    }
+}
