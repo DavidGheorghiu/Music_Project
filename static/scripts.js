d3c453241d35530675c5926889e77ec4e793a7f6
DavidGheorghiu <Pvt.david@yahoo.com>
2019-12-04 22:51:05 +0000

    commented some code

--- static/scripts.js
+++ static/scripts.js
@@ -1,21 +1,21 @@
-function play() {
-    var synth = new Tone.Synth().toMaster();
-    synth.triggerAttackRelease('C4', '8n');
-}
+// function play() {
+//     var synth = new Tone.Synth().toMaster();
+//     synth.triggerAttackRelease('C4', '8n');
+// }
 
-function loop() {
-    var synth = new Tone.Synth().toMaster();
-    var loop = new Tone.Loop(function(time){
-        synth.triggerAttackRelease("C4", "8n", time);
-    }, "4n");
+// function loop() {
+//     var synth = new Tone.Synth().toMaster();
+//     var loop = new Tone.Loop(function(time){
+//         synth.triggerAttackRelease("C4", "8n", time);
+//     }, "4n");
 
-    loop.start('1m').stop('4m');
-    Tone.Transport.start();
-}
+//     loop.start('1m').stop('4m');
+//     Tone.Transport.start();
+// }
 
-function stop() {
-    Tone.Transport.stop();
-}
+// function stop() {
+//     Tone.Transport.stop();
+// }
 
 var sequencer = document.querySelector('.sequencer');
 sequencer.addEventListener('click', function(event) {
