32b6caaaf884786fa7bebc211dc6405d8535f45f
basil roy <basil.k.roy@hotmail.com>
2019-12-16 14:06:43 +0000

    .

--- static/javascript/scripts.js
+++ static/javascript/scripts.js
@@ -18,6 +18,20 @@ function selectedWave() {
   synth.oscillator.type = val;
 }
 
+function selectedDistortion() {
+  var val = document.getElementById("distortion").value; //selected value of wave type
+  dist.distortion = val;
+}
+
+function selectedWet() {
+  var val = document.getElementById("wet").value; //selected value of wave type
+  dist.wet = val;
+}
+function selectedWave() {
+  var val = document.getElementById("selectedVal").value; //selected value of wave type
+  synth.oscillator.type = val;
+}
+
 function volumeGain(value) {
   synthVolume.volume.value = value;
 }
@@ -27,11 +41,20 @@ function Reverb(value) {
 }
 
 function volumeGainSampler(value) {
-  var vol = document.getElementById("volumeSampler").innerHTML = value;
+  var vol = (document.getElementById("volumeSampler").innerHTML = value);
 }
 
 function ReverbSampler(value) {
   document.getElementById("reverbSampler").innerHTML = value;
+}
+function selectedDistortionSampler() {
+  var val = document.getElementById("distortionSampler").value; //selected value of wave type
+  distSampler.distortion = val;
+}
+
+function selectedWetSampler() {
+  var val = document.getElementById("wetSampler").value; //selected value of wave type
+  distSampler.wet = val;
 }
 
 var sequencers = document.querySelectorAll(".sequencer");
@@ -49,22 +72,20 @@ for (sequencer of sequencers) {
   });
 }
 
-
-
-
 // Oscillator Sequencer
 {
-
   var synth = new Tone.Synth({
     detune: 0,
     oscillator: {
-      type: document.getElementById('selectedVal').value
+      type: document.getElementById("selectedVal").value
     }
   }).toMaster();
 
   // Effects
   var volumeSlider = document.getElementById("volumeValue").value;
   var reverbSlider = document.getElementById("reverbValue").value;
+  var distortionSlider = document.getElementById("distortion").value;
+  var wetSlider = document.getElementById("wet").value;
 
   var synthReverb = new Tone.Freeverb(reverbSlider).toMaster();
   synth.connect(synthReverb);
@@ -75,27 +96,36 @@ for (sequencer of sequencers) {
   }).toMaster();
   synth.connect(synthVolume);
 
+  var dist = new Tone.Distortion({
+    distortion: distortionSlider,
+    wet: wetSlider
+  }).toMaster();
+  synth.connect(dist);
+
   // Loop through the sequencer and play any sounds that were selected to play
   let cMajor = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
   let columns = document.getElementById("oscillator-sequencer").children;
 
-  var oscillatorSequence = new Tone.Sequence( function(time, columnIndex) {
-    // Get the current column
-    var column = columns[columnIndex];
-    // Get cells from the currently looped column and convert the htmlcollection to an array so we have access to the index values
-    var cells = Array.from(column.children);
+  var oscillatorSequence = new Tone.Sequence(
+    function(time, columnIndex) {
+      // Get the current column
+      var column = columns[columnIndex];
+      // Get cells from the currently looped column and convert the htmlcollection to an array so we have access to the index values
+      var cells = Array.from(column.children);
 
-    for (let cell of cells) {
-      if (cell.classList.contains("selected")) {
-        synth.triggerAttackRelease(cMajor[cells.indexOf(cell)], "32n");
+      for (let cell of cells) {
+        if (cell.classList.contains("selected")) {
+          synth.triggerAttackRelease(cMajor[cells.indexOf(cell)], "32n");
+        }
       }
-    }
-  }, [0, 1, 2, 3, 4, 5, 6, 7], "16n");
+    },
+    [0, 1, 2, 3, 4, 5, 6, 7],
+    "16n"
+  );
 }
 
 // Sample Sequencer
 {
-
   // Store the .wav files in a Tone player to be used in the Sequence
   var players = new Tone.Players(
     [
@@ -109,16 +139,19 @@ for (sequencer of sequencers) {
       "./static/Samples/909 Drum Machine/909YTOMS/909Y41FTL1.wav"
     ],
     {
-      volume: volumeSliderSampler,
+      // volume: volumeSliderSampler,
       mute: false,
       fadeOut: "64n"
     }
   ).toMaster();
 
-
   // Effects
   var volumeSliderSampler = document.getElementById("volumeValueSampler").value;
   var reverbSliderSampler = document.getElementById("reverbValueSampler").value;
+
+  var distortionSliderSampler = document.getElementById("distortionSampler")
+    .value;
+  var wetSliderSampler = document.getElementById("wetSampler").value;
 
   var samplerReverb = new Tone.Freeverb(reverbSliderSampler).toMaster();
   players.connect(samplerReverb);
@@ -128,6 +161,12 @@ for (sequencer of sequencers) {
     mute: false
   }).toMaster();
   players.connect(samplerVolume);
+
+  var distSampler = new Tone.Distortion({
+    distortion: distortionSliderSampler,
+    wet: wetSliderSampler
+  }).toMaster();
+  synth.connect(distSampler);
 
   // Loop through the sequencer and play any sounds that were selected to play
   let columns = document.getElementById("sample-sequencer").children;
